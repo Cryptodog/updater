d20d3878905de48a9774d6be094a31d41d3dc1e7
ayyghost <ayyghost@users.noreply.github.com>
2023-12-20 16:01:19 +0000

    validate target names, refactoring

---
+++ config.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+type Config struct {
+	DeployDir                       string    `json:"deploy_dir"`
+	Targets                         []*Target `json:"targets"`
+	PublicSigningKey                string    `json:"public_signing_key"`
+	UnsafeSkipSignatureVerification bool      `json:"unsafe_skip_signature_verification"`
+	UpdateInterval                  int       `json:"update_interval"`
+}
+
+type Target struct {
+	Name  string
+	Owner string
+	Repo  string
+}
+
+func (c *Config) Validate() error {
+	if c.DeployDir == "" {
+		return fmt.Errorf("deploy directory must be set")
+	}
+	if c.UpdateInterval <= 0 {
+		return fmt.Errorf("update interval must be >0")
+	}
+	if !c.UnsafeSkipSignatureVerification && c.PublicSigningKey == "" {
+		return fmt.Errorf("public signing key must be set if signature verification is enabled")
+	}
+	if len(c.Targets) == 0 {
+		return fmt.Errorf("at least one target must be set")
+	}
+
+	targetNames := make(map[string]bool)
+	targetNameRegex := regexp.MustCompile(`^[\w-]+$`)
+	for i, target := range c.Targets {
+		if target.Name == "" {
+			return fmt.Errorf("name for target %d must be set", i)
+		}
+		if !targetNameRegex.MatchString(target.Name) {
+			return fmt.Errorf("name for target %d must match pattern %s", i, targetNameRegex.String())
+		}
+		if target.Owner == "" {
+			return fmt.Errorf("owner for target %d must be set", i)
+		}
+		if target.Repo == "" {
+			return fmt.Errorf("repo for target %d must be set", i)
+		}
+		if targetNames[target.Name] {
+			return fmt.Errorf("target %d has duplicate name", i)
+		}
+		targetNames[target.Name] = true
+	}
+	return nil
+}
--- updater.go
+++ updater.go
@@ -20,20 +20,6 @@ import (
 	"github.com/jedisct1/go-minisign"
 )
 
-type Target struct {
-	Name  string
-	Owner string
-	Repo  string
-}
-
-type Config struct {
-	DeployDir                       string    `json:"deploy_dir"`
-	Targets                         []*Target `json:"targets"`
-	PublicSigningKey                string    `json:"public_signing_key"`
-	UnsafeSkipSignatureVerification bool      `json:"unsafe_skip_signature_verification"`
-	UpdateInterval                  int       `json:"update_interval"`
-}
-
 func main() {
 	configFile := flag.String("config", "config.json", "path to config file")
 	flag.Parse()
@@ -113,38 +99,6 @@ func main() {
 		}
 		time.Sleep(time.Duration(config.UpdateInterval) * time.Second)
 	}
-}
-
-func (c *Config) Validate() error {
-	if c.DeployDir == "" {
-		return fmt.Errorf("deploy directory must be set")
-	}
-	if c.UpdateInterval <= 0 {
-		return fmt.Errorf("update interval must be >0")
-	}
-	if !c.UnsafeSkipSignatureVerification && c.PublicSigningKey == "" {
-		return fmt.Errorf("public signing key must be set if signature verification is enabled")
-	}
-	if len(c.Targets) == 0 {
-		return fmt.Errorf("at least one target must be set")
-	}
-	targetNames := make(map[string]bool)
-	for i, target := range c.Targets {
-		if target.Name == "" {
-			return fmt.Errorf("name for target %d must be set", i)
-		}
-		if target.Owner == "" {
-			return fmt.Errorf("owner for target %d must be set", i)
-		}
-		if target.Repo == "" {
-			return fmt.Errorf("repo for target %d must be set", i)
-		}
-		if targetNames[target.Name] {
-			return fmt.Errorf("target %d has duplicate name", i)
-		}
-		targetNames[target.Name] = true
-	}
-	return nil
 }
 
 func getReleaseDir(deployDir, targetName, releaseID string) string {
